d78fc28723a42dc48ebb3a9f0eb620ad8551b1f8
Matteo Giordano <matteo.giordano@eng.it>
2024-06-06 17:22:06 +0200

    implement API calls

--- src/cloudesire_client.rs
+++ src/cloudesire_client.rs
@@ -1,6 +1,7 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::env;
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "UPPERCASE")]
 pub enum DeploymentStatus {
     Pending,
@@ -18,15 +19,33 @@ pub struct Subscription {
 }
 
 pub fn get_subscription(id: u32) -> Subscription {
-    Subscription {
-        id,
-        deployment_status: DeploymentStatus::Pending,
-        paid: false,
-    }
+    let base_url = env::var("CMW_BASE_URL").unwrap_or("http://localhost:8081".to_string());
+    let auth_token = env::var("CMW_AUTH_TOKEN").unwrap_or("test-token".to_string());
+
+    let url = base_url + "/subscription/" + &id.to_string();
+    ureq::get(&url)
+        .set("CMW-Auth-Token", &auth_token)
+        .call()
+        .unwrap()
+        .into_json()
+        .unwrap()
 }
 
 pub fn update_status(subscription_id: u32, status: DeploymentStatus) {
     use log::info;
 
     info!("Setting subscription {} to {:?}", subscription_id, status);
+
+    if env::var("CMW_READ_ONLY").is_ok() {
+        return;
+    }
+
+    let base_url = env::var("CMW_BASE_URL").unwrap_or("http://localhost:8081".to_string());
+    let auth_token = env::var("CMW_AUTH_TOKEN").unwrap_or("test-token".to_string());
+
+    let url = base_url + "/subscription/" + &subscription_id.to_string();
+    ureq::patch(&url)
+        .set("CMW-Auth-Token", &auth_token)
+        .send_json(ureq::json!({"deploymentStatus": status}))
+        .unwrap();
 }
